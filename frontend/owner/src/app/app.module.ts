bd09d57c86edde12d9fb0883faa16de7202ba0fe
Le Hong Quan <lequan18042001@gmail.com>
2024-06-13 10:09:13 +0700

    Update layouts and page in src

--- frontend/owner/src/app/app.module.ts
+++ frontend/owner/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layouts/header/header.component';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { MenuComponent } from './layouts/menu/menu.component';
+import { NavComponent } from './layouts/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailPostComponent } from './pages/detail-post/detail-post.component';
+import { UpdatePostComponent } from './pages/update-post/update-post.component';
+import { CreatePostComponent } from './pages/create-post/create-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    NavComponent,
+    HomeComponent,
+    DetailPostComponent,
+    UpdatePostComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
