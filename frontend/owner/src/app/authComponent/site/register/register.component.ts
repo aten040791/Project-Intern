9aade29f0f7609737b4b0db344b27cae67e91949
Le Hong Quan <lequan18042001@gmail.com>
2024-06-19 17:24:41 +0700

    Update(Template): Response data auth form in template angular

--- frontend/owner/src/app/authComponent/site/register/register.component.ts
+++ frontend/owner/src/app/authComponent/site/register/register.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  postForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.postForm = this.fb.group({
+      username: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    if (this.postForm.valid) {
+      console.log(this.postForm.value);
+      // Handle form submission logic here
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
 }
