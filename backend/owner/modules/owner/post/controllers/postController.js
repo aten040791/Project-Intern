7842b39e5ce839ad7d702014620505ce5df558a0
Le Hong Quan <lequan18042001@gmail.com>
2024-07-31 17:30:23 +0700

    refactor code

--- backend/owner/modules/owner/post/controllers/postController.js
+++ backend/owner/modules/owner/post/controllers/postController.js
@@ -30,7 +30,6 @@ const postController = {
     try {
       const { uid } = req.params;
       const { keyword, categoryId, status, page = 1, perPage = 10 } = req.query;
-      console.log(req.query);
       const posts = await postService.getByUid(uid, keyword, categoryId, status, parseInt(page), parseInt(perPage));
       return responseUtils.ok(res, posts);
     } catch (error) {
@@ -80,7 +79,6 @@ const postController = {
       await postService.delete(idsPost);
       return responseUtils.ok(res, { message: "Posts deleted successfully" });
     } catch (error) {
-      console.error("Error deleting posts:", error);
       return responseUtils.error(res, "Failed to delete posts.");
     }
   },
